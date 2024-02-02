d9e836873b0f5032078e5b57ad125e6e914498ce
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-02 23:27:58 +0600

    categories get api created

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors')
+app.use(cors())
+
+const categories = require('./date/categories.json')
 
 app.get('/', (req, res) => {
     res.send("Dragon is running")
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log(`Dragon API running on port: ${port}`)
