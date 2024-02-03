993fd605eb0e64bd672d699a7d482e26f2aa9a6e
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-03 12:04:41 +0600

    basic server setUp practice done

--- index.js
+++ index.js
@@ -1,19 +1,112 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const port = 5000;
+
+// const cors = require('cors')
+// app.use(cors())
+
+
+// const categories = require('./date/categories.json')
+
+
+// app.get('/', (req, res) => {
+//     res.send("Dragon is running")
+// });
+
+
+
+// app.get('/categories', (req, res) => {
+//     res.send(categories)
+// })
+
+
+
+// app.listen(port, () => {
+//     console.log(`Dragon API running on port: ${port}`)
+// })
+//-------------------------End------------------------------
+
+
+
+
+
+
+// const express = require("express");
+// const app = express();
+// const port = 5000;
+
+// const cors = require("cors");
+// app.use(cors())
+
+
+// const categories = require('./date/categories.json');
+
+
+// app.get("/", (req, res) => {
+//     res.send("Dragon news is Running Now!!!")
+// })
+
+
+// app.get('/categories', (req, res) => {
+//     res.send(categories)
+// })
+
+
+// app.listen(port, () => {
+//     console.log(`Dragon news is running on port2: ${port}`)
+// })
+//----------------------------End----------------------------
+
+
+// const express = require("express");
+// const app = express();
+// const port = 5000;
+
+// const cors = require("cors");
+// app.use(cors());
+
+
+// const categories = require("./data/categories.json")
+
+
+// app.get("/", (req, res) => {
+//     res.send("Dragon News 3 is Running");
+// });
+
+
+// app.get("/categories", (req, res) => {
+//     res.send(categories)
+// })
+
+
+// app.listen(port, () => {
+//     console.log(`Dragon News 3 is Running on Port:${port}`)
+// });
+//-----------------------------End-------------------------------
+
+
+const express = require("express");
 const app = express();
 const port = 5000;
-const cors = require('cors')
-app.use(cors())
 
-const categories = require('./date/categories.json')
+const cors = require("cors");
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Dragon is running")
+
+const categories = require("./data/categories.json");
+
+
+app.get("/", (req, res) => {
+    res.send("Dragon News 4 is Running");
 });
 
-app.get('/categories', (req, res) => {
+
+app.get("/categories", (req, res) => {
     res.send(categories)
-})
+});
+
 
 app.listen(port, () => {
-    console.log(`Dragon API running on port: ${port}`)
-})
+    console.log(`Dragon News 4 is running on Port:${port}`)
+});
+//------------------End---------------------------
