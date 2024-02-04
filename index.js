24ad1618bb9a25147203e73344759f7518839156
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-04 22:27:06 +0600

    basic server practice again

--- index.js
+++ index.js
@@ -85,6 +85,32 @@
 //-----------------------------End-------------------------------
 
 
+// const express = require("express");
+// const app = express();
+// const port = 5000;
+
+// const cors = require("cors");
+// app.use(cors());
+
+
+// const categories = require("./data/categories.json");
+
+
+// app.get("/", (req, res) => {
+//     res.send("Dragon News 4 is Running");
+// });
+
+
+// app.get("/categories", (req, res) => {
+//     res.send(categories)
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`Dragon News 4 is running on Port:${port}`)
+// });
+//------------------End---------------------------
+
 const express = require("express");
 const app = express();
 const port = 5000;
@@ -109,4 +135,3 @@ app.get("/categories", (req, res) => {
 app.listen(port, () => {
     console.log(`Dragon News 4 is running on Port:${port}`)
 });
-//------------------End---------------------------
